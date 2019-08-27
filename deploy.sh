a2281577cf3385942883836d20838783ff728820
machsix <28209092+machsix@users.noreply.github.com>
2019-08-27 18:18:42 -0400

    fix  travis [skip ci]

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,12 @@ set +e
 export TOKEN=$1
 # upload rpm back to github
 export COMMITER=`git log -1 --pretty=format:'%an'`
-if [ $COMMITER != "Travis CI" ]; then
+if [[ $COMMITER != "Travis CI" ]]; then
   git remote set-url origin https://machsix:${TOKEN}@github.com/machsix/WDMyCloud-Gen2.git
   git fetch
   git config --global user.email "travis@travis-ci.com"
   git config --global user.name "Travis CI"
   git add Release || true
-  git commit -m "Pack ${TRAVIS_COMMIT}"
+  git commit -m "Pack ${TRAVIS_COMMIT} [skip ci]"
   git push origin HEAD:master
 fi
