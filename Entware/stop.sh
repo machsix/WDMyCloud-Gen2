f035eaf399a55f25177b86b3ae4d237f37876045
machsix <28209092+machsix@users.noreply.github.com>
2019-10-03 14:23:00 -0400

    fix: lower case naming

--- Entware/stop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-sed -i '/export PATH=$PATH:\/opt\/bin:\/opt\/sbin:\/opt\/usr\/bin/d' /home/root/.profile
-/opt/etc/init.d/rc.unslung stop
---
+++ entware/remove.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+INSTALL_DIR=$1
+
+sed -i '/export PATH=$PATH:\/opt\/bin:\/opt\/sbin:\/opt\/usr\/bin/d' /home/root/.profile
+/opt/etc/init.d/rc.unslung stop
+
+# delete link
+rm -f /opt
+
+# copy file back
+cp -av $INSTALL_DIR/opt_bak /opt
+rm -rf /var/www/entware
+rm -rf $INSTALL_DIR
